446b0fe2647ea845408efe7c32b5a949d74a2158
rizalludin <dodirizal5@gmail>
2018-10-30 22:54:55 +0700

    update di echo pertama

--- tugasPHP1.php
+++ tugasPHP1.php
@@ -5,7 +5,7 @@
 </head>
 <body bgcolor="gray">
 	<?php
-	echo '<p style="font-family:"Helvetica"">'."Mati satu tinggal $i".'</p>';
+	echo '<p style="font-family:"Helvetica"">'."Tekotek-kotek anak ayam turun 100".'</p>';
 	for ($i=99; $i>=1 ; $i--) {
 		if($i%2==0){
 			echo '<p style="color:brown; font-family:"Helvetica"">'."Mati satu tinggal $i".'</p>';
