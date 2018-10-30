712272a6532dbf28bc9617b0d85d6a7aa2e3e9b0
rizalludin <dodirizal5@gmail>
2018-10-30 22:52:13 +0700

    update huruf ke helevetica

--- tugasPHP1.php
+++ tugasPHP1.php
@@ -5,8 +5,7 @@
 </head>
 <body bgcolor="gray">
 	<?php
-	echo "TEKOTEK-OTEK anak ayam turun 100";
-	echo "<br>";
+	echo '<p style="font-family:"Helvetica"">'."Mati satu tinggal $i".'</p>';
 	for ($i=99; $i>=1 ; $i--) {
 		if($i%2==0){
 			echo '<p style="color:brown; font-family:"Helvetica"">'."Mati satu tinggal $i".'</p>';
